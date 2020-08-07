63dab0951d073a0c1c391b4e943f89ab6615a195
happy <1475977956@qq.com>
2020-08-07 19:07:41 +0800

    Initial commit

---
+++ src/main/java/net/xm/order_service/domain/ProductOrder.java
@@ -0,0 +1,75 @@
+package net.xm.order_service.domain;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ProductOrder  implements Serializable {
+
+
+    private int id;
+    private String productName;//商品名称
+    private String tradeNo;//订单号
+    private int price;//价格，分
+    private Date createTime;
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getTradeNo() {
+        return tradeNo;
+    }
+
+    public void setTradeNo(String tradeNo) {
+        this.tradeNo = tradeNo;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
